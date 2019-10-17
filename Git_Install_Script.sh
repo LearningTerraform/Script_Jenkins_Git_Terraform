9b6c87333e6f4efa955da488db965c35b23265e2
vvijay777 <sftesting7777@gmail.com>
2019-10-17 20:35:43 +0530

    Scripts

---
+++ Git_Install_Script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo yum update -y
+#Install git in your EC2 instance
+sudo yum install git -y
+#Check git version
+git version
---
+++ Terraform_Install.sh
@@ -0,0 +1,4 @@
+#### Be Root while Running the Script
+#!/bin/bash
+curl -O https://releases.hashicorp.com/terraform/0.12.10/terraform_0.12.10_linux_amd64.zip
+unzip terraform_0.12.10_linux_amd64.zip -d /usr/bin
